ab3b9cd600afba39822e3355a551cb09e35f5079
viveks2903 <viveks10482@gmail.com>
2025-11-27 22:21:14 +0530

    used 2 pointers approach

---
+++ add_binary.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int bitA = (i >= 0) ? a.charAt(i) - '0' : 0;
+            int bitB = (j >= 0) ? b.charAt(j) - '0' : 0;
+
+            int sum = bitA + bitB + carry;
+            sb.append(sum % 2);
+            carry = sum / 2;
+
+            i--;
+            j--;
+        }
+
+        return sb.reverse().toString();
+    }
+}
