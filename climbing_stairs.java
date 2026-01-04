0d3cf81e59f003c28020e51cb02b844ec5fa85da
viveks2903 <viveks10482@gmail.com>
2026-01-04 22:16:07 +0530

    used fibonacci logic

---
+++ climbing_stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1)return 1;
+        if(n==2) return 2;
+        int prev2=1;
+        int prev1=2;
+
+        for(int i=3;i<=n;i++)
+        {
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        return prev1;
+    }
+}
